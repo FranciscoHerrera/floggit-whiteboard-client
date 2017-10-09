8819f6067b9eb4ec4c170090412369475ae00ae6
Malin Wetter <malinwetter@Malins-MacBook-Pro-2.local>
2017-10-09 13:52:32 +0200

    addedfiles for whiteboardlist component

--- src/common/components/WhiteboardContainer/components/WhiteboardList/WhiteboardList.props.js
+++ src/common/components/WhiteboardContainer/components/WhiteboardList/WhiteboardList.props.js
@@ -1,1 +1,15 @@
 import PropTypes from 'prop-types';
+
+const propTypes = {
+  postIts: PropTypes.arrayOf(PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    infoList: PropTypes.string.isRequired,
+    authorName: PropTypes.string.isRequired,
+    color: PropTypes.string.isRequired,
+  })),
+  onItemRemove: PropTypes.func.isRequired,
+  onItemUpdate: PropTypes.func.isRequired,
+};
+
+export default propTypes;
