cc2286e43fd86a07578d81bc067917a0edb56d88
Malin Wetter <malinwetter@Malins-MBP-2.lan>
2017-10-12 20:06:27 +0200

    added hide/show on add-button and moved sign on post-it

--- src/common/components/WhiteboardContainer/components/WhiteboardInput/WhiteboardInput.js
+++ src/common/components/WhiteboardContainer/components/WhiteboardInput/WhiteboardInput.js
@@ -9,16 +9,19 @@ const WhiteboardInput = (props) => {
   let authorName;
   let color;
   let visible;
+  let editVisible;
 
   const hideAdd = () => {
     visible.style.display = 'none';
+    editVisible.style.display = 'block';
   };
 
   const showAdd = () => {
     visible.style.display = 'block';
+    editVisible.style.display = 'none';
   };
 
-  const handleClick = () => {
+  const handleAdd = () => {
     props.onAdd(title.value, infoList.value, authorName.value, color.value);
     title.value = '';
     infoList.value = '';
@@ -29,7 +32,10 @@ const WhiteboardInput = (props) => {
 
   return (
     <div className="create-postIt">
-      <button type="button" onClick={showAdd}>
+      <button
+        ref={(currentElement) => { editVisible = currentElement; }}
+        onClick={showAdd}
+      >
       + Add
       </button>
       <div
@@ -60,9 +66,8 @@ const WhiteboardInput = (props) => {
           <option value={COLOR_4}>{COLOR_NAME_4}</option>
         </select>
         <button
-          type="button"
           id="save"
-          onClick={handleClick}
+          onClick={handleAdd}
         >
           Save
         </button>
